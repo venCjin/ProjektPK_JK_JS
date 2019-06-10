e28be61a23ca844161ed87a4a1aaedb41276c1c6
Jarosław Suchiński <ven.cjin@gmail.com>
2019-06-10 17:35:20 +0200

    Zmiany. Dodane sprawozdanie do uzupełniania.

--- src/org/organizer/SearchWindow.java
+++ src/org/organizer/SearchWindow.java
@@ -37,7 +37,6 @@ public class SearchWindow {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
